a65f6bdac5be3ef279e2fffbb16f0f657fe6eb59
JustinD-Dev <39993170+justind-dev@users.noreply.github.com>
2022-01-03 11:35:02 -0500

    Formatting / Indentation Fixed
    Fixed formatting and indentation for readability.

--- Form1.cs
+++ Form1.cs
@@ -3,27 +3,27 @@ using RoboSharp;
 
 namespace RoboMigratorGUI;
 
-    public partial class Form1 : Form
-    {
+public partial class Form1 : Form
+{
 
-        private RoboSharp.Results.RoboCopyResultsList JobResults = new RoboSharp.Results.RoboCopyResultsList();
-        RoboSharp.RoboQueue roboQueue = new RoboSharp.RoboQueue();
-        Stopwatch copyTime = new Stopwatch();
-        
+    private RoboSharp.Results.RoboCopyResultsList JobResults = new RoboSharp.Results.RoboCopyResultsList();
+    RoboSharp.RoboQueue roboQueue = new RoboSharp.RoboQueue();
+    Stopwatch copyTime = new Stopwatch();
+
 
     public Form1()
-        {
-            InitializeComponent();
-        }
+    {
+        InitializeComponent();
+    }
     void Backup_OnBackupCommandCompletion(object sender, RoboCommandCompletedEventArgs e)
-        {
+    {
 
 
-            var results = e.Results;
-            Console.WriteLine("Files copied: " + results.FilesStatistic.Copied);
-            Console.WriteLine("Directories copied: " + results.DirectoriesStatistic.Copied);
-            JobResults.Add(e.Results);
-        }
+        var results = e.Results;
+        Console.WriteLine("Files copied: " + results.FilesStatistic.Copied);
+        Console.WriteLine("Directories copied: " + results.DirectoriesStatistic.Copied);
+        JobResults.Add(e.Results);
+    }
     private RoboCommand GetCommand(bool BindEvents, string jobSourceDirectory, string jobDestinationDirectory)
     {
         RoboCommand backup = new RoboCommand();
@@ -69,99 +69,99 @@ namespace RoboMigratorGUI;
         return backup;
     }
     private void CreateJobs()
+    {
+        var _jobs = new Dictionary<string, string>();
+        List<RoboCommand> _jobList = new List<RoboCommand>();
+        RoboSharp.RoboQueue roboQueue = new RoboSharp.RoboQueue();
+        foreach (var sourceSubDirectory in ProcessDirectory(SourceTextBox.Text))
+
         {
-            var _jobs = new Dictionary<string, string>();
-            List<RoboCommand> _jobList = new List<RoboCommand>();
-            RoboSharp.RoboQueue roboQueue = new RoboSharp.RoboQueue();
-            foreach (var sourceSubDirectory in ProcessDirectory(SourceTextBox.Text))
-
-            {
-                if (sourceSubDirectory == null) continue;
-                var directoryPaths = sourceSubDirectory.Split('\\', StringSplitOptions.RemoveEmptyEntries);
-                var currentFolderName = directoryPaths.Last();
-                var destinationSubDirectory = DestinationTextBox.Text + @"\" + currentFolderName;
-                if (!roboQueue.IsRunning)
-                {
-                    roboQueue.AddCommand(GetCommand(false, sourceSubDirectory, destinationSubDirectory));
-                }
-
-            }
-            // add job for root directory files
+            if (sourceSubDirectory == null) continue;
+            var directoryPaths = sourceSubDirectory.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+            var currentFolderName = directoryPaths.Last();
+            var destinationSubDirectory = DestinationTextBox.Text + @"\" + currentFolderName;
             if (!roboQueue.IsRunning)
             {
-                roboQueue.AddCommand(GetCommand(false, SourceTextBox.Text, DestinationTextBox.Text));
+                roboQueue.AddCommand(GetCommand(false, sourceSubDirectory, destinationSubDirectory));
             }
-        }
-        private static string[] ProcessDirectory(string targetDirectory)
-        {
-            if (Directory.Exists(targetDirectory))
-            {
-                var subdirectoryEntries = Directory.GetDirectories(targetDirectory);
 
-                return subdirectoryEntries;
-            }
-            else
-            {
-                return null;
-            }
         }
-        private void DisplayCopyInformation(Stopwatch copyTimer)
+        // add job for root directory files
+        if (!roboQueue.IsRunning)
         {
-            var message = "Migration completed in:\n" +
-                          "Hours: " + copyTimer.Elapsed.Hours + "\n" +
-                          "Seconds: " + copyTimer.Elapsed.Seconds + "\n" +
-                          "Milliseconds: " + copyTimer.Elapsed.Milliseconds + "\n" +
-                          "Time Completed: " + DateTime.Now.ToString();
-
-            var caption = "Migration Completed";
-            var buttons = MessageBoxButtons.OK;
-            MessageBox.Show(message, caption, buttons);
+            roboQueue.AddCommand(GetCommand(false, SourceTextBox.Text, DestinationTextBox.Text));
         }
-        
+    }
+    private static string[] ProcessDirectory(string targetDirectory)
+    {
+        if (Directory.Exists(targetDirectory))
+        {
+            var subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+
+            return subdirectoryEntries;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    private void DisplayCopyInformation(Stopwatch copyTimer)
+    {
+        var message = "Migration completed in:\n" +
+                      "Hours: " + copyTimer.Elapsed.Hours + "\n" +
+                      "Seconds: " + copyTimer.Elapsed.Seconds + "\n" +
+                      "Milliseconds: " + copyTimer.Elapsed.Milliseconds + "\n" +
+                      "Time Completed: " + DateTime.Now.ToString();
+
+        var caption = "Migration Completed";
+        var buttons = MessageBoxButtons.OK;
+        MessageBox.Show(message, caption, buttons);
+    }
+
     private async void StartMigration()
     {
         copyTime.Start();
         await roboQueue.StartAll();
         copyTime.Stop();
     }
-        private void button1_Click(object sender, EventArgs e)
-        {
+    private void button1_Click(object sender, EventArgs e)
+    {
         roboQueue.MaxConcurrentJobs = 8;
-            if (!roboQueue.IsRunning)
+        if (!roboQueue.IsRunning)
+        {
+            if (SourceTextBox.Text != "" | DestinationTextBox.Text != "")
             {
-                if (SourceTextBox.Text != "" | DestinationTextBox.Text != "")
-                    {
-                    CreateJobs();
-                    StartMigration();
-                    }
-                else
-                    {
-                        MessageBox.Show("Please enter a value for both source and destination.");
-                    }
+                CreateJobs();
+                StartMigration();
             }
             else
-                {
+            {
+                MessageBox.Show("Please enter a value for both source and destination.");
+            }
+        }
+        else
+        {
             MessageBox.Show("There is already a migration in progress,\nplease wait until that completes.");
-                }
-        
+        }
+
 
     }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
+    private void label3_Click(object sender, EventArgs e)
+    {
 
-        }
+    }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            var compare = new DirCompare();
-            compare.CompareDirectories(SourceTextBox.Text, DestinationTextBox.Text);
-        }
+    private void button2_Click(object sender, EventArgs e)
+    {
+        var compare = new DirCompare();
+        compare.CompareDirectories(SourceTextBox.Text, DestinationTextBox.Text);
+    }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+    private void Form1_Load(object sender, EventArgs e)
+    {
 
-        }
+    }
 
 
 }
